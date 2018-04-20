1bd3f294c184ca184082ec09f5fbaa17f52a1b86
Jeremy Ma <junweima2@gmail.com>
2018-04-20 11:02:58 -0400

    small fix

--- Text-to-Image-Synthesis/models/gan_cls.py
+++ Text-to-Image-Synthesis/models/gan_cls.py
@@ -184,88 +184,88 @@ class discriminator(nn.Module):
         return x.view(-1, 1).squeeze(1) , x_intermediate
 
 
-    class generator2(nn.Module):
-        def __init__(self):
-            super(generator2, self).__init__()
-            self.image_size = 128
-            self.num_channels = 3
-            self.noise_dim = 100
-            self.embed_dim = 2400 # compatible with skip thought (1024)
-            self.projected_embed_dim = 128
-            self.latent_dim = self.noise_dim + self.projected_embed_dim
-            self.ngf = 64
+class generator2(nn.Module):
+    def __init__(self):
+        super(generator2, self).__init__()
+        self.image_size = 128
+        self.num_channels = 3
+        self.noise_dim = 100
+        self.embed_dim = 2400 # compatible with skip thought (1024)
+        self.projected_embed_dim = 128
+        self.latent_dim = self.noise_dim + self.projected_embed_dim
+        self.ngf = 64
 
-            self.projection = nn.Sequential(
-                nn.Linear(in_features=self.embed_dim, out_features=self.projected_embed_dim),
-                nn.BatchNorm1d(num_features=self.projected_embed_dim),
-                nn.LeakyReLU(negative_slope=0.2, inplace=True)
-            )
-
-
-            # downsample
-            self.netG_1 = nn.Sequential(
-                # input is (nc) x 64 x 64
-                nn.Conv2d(self.num_channels, self.ngf, 3, 1, 1),
-                nn.ReLU(True),
-                # state size. (ngf) x 64 x 64
-                nn.Conv2d(self.ngf, self.ngf * 2, 4, 2, 1, bias=False),
-                nn.BatchNorm2d(self.ngf * 2),
-                nn.ReLU(True),
-                # state size. (ngf*2) x 32 x 32
-                nn.Conv2d(self.ngf * 2, self.ngf * 4, 4, 2, 1, bias=False),
-                nn.BatchNorm2d(self.ngf * 4),
-                nn.ReLU(True)
-                # state size. (ngf*4) x 16 x 16
-            )
-
-            self.join_embed = nn.Sequential(
-
-                nn.Conv2d(self.projected_embed_dim + self.ngf*4 , self.ngf * 4, 3, 1, 1),
-                nn.BatchNorm2d(self.ngf * 4),
-                nn.ReLU(True)
-            )
-
-            self.residual = self._make_layer(ResBlock, self.ngf * 4, 4)
-
-            # --> 2ngf x 32 x 32
-            self.upsample1 = upBlock(self.ngf * 4, self.ngf * 2)
-            # --> ngf x 64 x 64
-            self.upsample2 = upBlock(self.ngf * 2, self.ngf)
-            # --> ngf // 2 x 128 x 128
-            self.upsample3 = upBlock(self.ngf, self.ngf // 2)
-            # --> ngf // 4 x 128 x 128
-            self.flatten = nn.Conv2d(self.ngf // 2 , self.ngf //4, 3, 1, 1)
-            # --> 3 x 128 x 128
-            self.img = nn.Sequential(
-                nn.Conv2d(self.ngf // 4 , 3, 3, 1, 1),
-                nn.Tanh()
-            )
-
-        def _make_layer(self, block, channel_num, r_num):
-            layers = []
-            for i in range(r_num):
-                layers.append(block(channel_num))
-            return nn.Sequential(*layers)
+        self.projection = nn.Sequential(
+            nn.Linear(in_features=self.embed_dim, out_features=self.projected_embed_dim),
+            nn.BatchNorm1d(num_features=self.projected_embed_dim),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True)
+        )
 
 
-        def forward(self, inp, embed):
+        # downsample
+        self.netG_1 = nn.Sequential(
+            # input is (nc) x 64 x 64
+            nn.Conv2d(self.num_channels, self.ngf, 3, 1, 1),
+            nn.ReLU(True),
+            # state size. (ngf) x 64 x 64
+            nn.Conv2d(self.ngf, self.ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.ngf * 2),
+            nn.ReLU(True),
+            # state size. (ngf*2) x 32 x 32
+            nn.Conv2d(self.ngf * 2, self.ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.ngf * 4),
+            nn.ReLU(True)
+            # state size. (ngf*4) x 16 x 16
+        )
 
-            # embed_vector = 64 by 1024
-            # projected_embed = 64 by 128 by 1 by 1
-            # z = 64 by 100 by 1 by 1
-            g1_output = self.netG_1(inp) # shape is (ngf*4) x 16 x 16
-            projected_embed = self.projection(embed)
-            replicated_embed = projected_embed.repeat(16, 16, 1, 1).permute(2, 3, 0, 1)
-            hidden_concat = torch.cat([g1_output, replicated_embed], 1)
-            x = self.join_embed(hidden_concat)
-            x = self.residual(x)
-            x = self.upsample1(x)
-            x = self.upsample2(x)
-            x = self.upsample3(x)
-            x = self.flatten(x)
-            output = self.img(x)
+        self.join_embed = nn.Sequential(
 
-            return output
+            nn.Conv2d(self.projected_embed_dim + self.ngf*4 , self.ngf * 4, 3, 1, 1),
+            nn.BatchNorm2d(self.ngf * 4),
+            nn.ReLU(True)
+        )
+
+        self.residual = self._make_layer(ResBlock, self.ngf * 4, 4)
+
+        # --> 2ngf x 32 x 32
+        self.upsample1 = upBlock(self.ngf * 4, self.ngf * 2)
+        # --> ngf x 64 x 64
+        self.upsample2 = upBlock(self.ngf * 2, self.ngf)
+        # --> ngf // 2 x 128 x 128
+        self.upsample3 = upBlock(self.ngf, self.ngf // 2)
+        # --> ngf // 4 x 128 x 128
+        self.flatten = nn.Conv2d(self.ngf // 2 , self.ngf //4, 3, 1, 1)
+        # --> 3 x 128 x 128
+        self.img = nn.Sequential(
+            nn.Conv2d(self.ngf // 4 , 3, 3, 1, 1),
+            nn.Tanh()
+        )
+
+    def _make_layer(self, block, channel_num, r_num):
+        layers = []
+        for i in range(r_num):
+            layers.append(block(channel_num))
+        return nn.Sequential(*layers)
+
+
+    def forward(self, inp, embed):
+
+        # embed_vector = 64 by 1024
+        # projected_embed = 64 by 128 by 1 by 1
+        # z = 64 by 100 by 1 by 1
+        g1_output = self.netG_1(inp) # shape is (ngf*4) x 16 x 16
+        projected_embed = self.projection(embed)
+        replicated_embed = projected_embed.repeat(16, 16, 1, 1).permute(2, 3, 0, 1)
+        hidden_concat = torch.cat([g1_output, replicated_embed], 1)
+        x = self.join_embed(hidden_concat)
+        x = self.residual(x)
+        x = self.upsample1(x)
+        x = self.upsample2(x)
+        x = self.upsample3(x)
+        x = self.flatten(x)
+        output = self.img(x)
+
+        return output
 
 class discriminator2(nn.Module):
     def __init__(self):
@@ -280,28 +280,28 @@ class discriminator2(nn.Module):
 
         self.encode_img = nn.Sequential(
             # state size = 3 x 128 x 128
-            nn.Conv2d(3, self.ndf, 4, 2, 1, bias=False),  # 128 * 128 * ndf
+            nn.Conv2d(3, self.ndf, 4, 2, 1, bias=True),  # 128 * 128 * ndf
             nn.LeakyReLU(0.2, inplace=True),
             # state size = ndf x 64 x 64
-            nn.Conv2d(self.ndf , self.ndf * 2, 3, 1, 1, bias=False),
+            nn.Conv2d(self.ndf , self.ndf * 2, 3, 1, 1, bias=True),
             nn.BatchNorm2d(self.ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(self.ndf * 2, self.ndf * 4, 4, 2, 1, bias=False),
+            nn.Conv2d(self.ndf * 2, self.ndf * 4, 4, 2, 1, bias=True),
             nn.BatchNorm2d(self.ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),  # 32 * 32 * ndf * 4
-            nn.Conv2d(self.ndf * 4, self.ndf * 8, 4, 2, 1, bias=False),
+            nn.Conv2d(self.ndf * 4, self.ndf * 8, 4, 2, 1, bias=True),
             nn.BatchNorm2d(self.ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),  # 16 * 16 * ndf * 8
-            nn.Conv2d(self.ndf * 8, self.ndf * 16, 4, 2, 1, bias=False),
+            nn.Conv2d(self.ndf * 8, self.ndf * 16, 4, 2, 1, bias=True),
             nn.BatchNorm2d(self.ndf * 16),
             nn.LeakyReLU(0.2, inplace=True),  # 8 * 8 * ndf * 16
-            nn.Conv2d(self.ndf * 16, self.ndf * 32, 4, 2, 1, bias=False),
+            nn.Conv2d(self.ndf * 16, self.ndf * 32, 4, 2, 1, bias=True),
             nn.BatchNorm2d(self.ndf * 32),
             nn.LeakyReLU(0.2, inplace=True),  # 4 * 4 * ndf * 32
             conv3x3(self.ndf * 32, self.ndf * 16),
             nn.BatchNorm2d(self.ndf * 16),
             nn.LeakyReLU(0.2, inplace=True),   # 4 * 4 * ndf * 16
-            nn.Conv2d(self.ndf * 16, self.ndf * 8, 3, 1, 1, bias=False),
+            nn.Conv2d(self.ndf * 16, self.ndf * 8, 3, 1, 1, bias=True),
             nn.BatchNorm2d(self.ndf * 8),
             nn.LeakyReLU(0.2, inplace=True)   # 4 * 4 * ndf * 8
         )
