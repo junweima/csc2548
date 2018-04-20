3dbf3a858a7a02f8de5fdf83b7481d2a9f369fcd
Jeremy Ma <junweima2@gmail.com>
2018-04-20 13:21:36 -0400

    txt

--- Text-to-Image-Synthesis/txt2image_dataset.py
+++ Text-to-Image-Synthesis/txt2image_dataset.py
@@ -9,6 +9,8 @@ import torch
 from torch.autograd import Variable
 import pdb
 import torch.nn.functional as F
+import pandas as pd
+import torchvision.transforms as transforms
 
 class Text2ImageDataset(Dataset):
 
@@ -18,6 +20,8 @@ class Text2ImageDataset(Dataset):
         self.dataset = None
         self.dataset_keys = None
         self.split = 'train' if split == 0 else 'valid' if split == 1 else 'test'
+        self.bboxes_df = pd.read_table('bounding_boxes.txt', sep=' ', header=None)
+        self.image_paths_df = pd.read_table('images.txt', sep='\s+|\/+', header=None)	
         self.h5py2int = lambda x: int(np.array(x))
 
     def __len__(self):
@@ -28,29 +32,75 @@ class Text2ImageDataset(Dataset):
 
         return length
 
+    def crop_image(self, img, bbox):
+        width, height = img.size
+        r = int(np.maximum(bbox[2], bbox[3]) * 0.75)
+        center_x = int((2 * bbox[0] + bbox[2]) / 2)
+        center_y = int((2 * bbox[1] + bbox[3]) / 2)
+        y1 = np.maximum(0, center_y - r)
+        y2 = np.minimum(height, center_y + r)
+        x1 = np.maximum(0, center_x - r)
+        x2 = np.minimum(width, center_x + r)
+        img = img.crop([x1, y1, x2, y2])
+        return img
+
+
     def __getitem__(self, idx):
         if self.dataset is None:
             self.dataset = h5py.File(self.datasetFile, mode='r')
             self.dataset_keys = [str(k) for k in self.dataset[self.split].keys()]
 
-        example_name = self.dataset_keys[idx]
+        # example_name = self.dataset_keys[idx]
+        example_name = self.dataset_keys[0]
         example = self.dataset[self.split][example_name]
+        index_found = self.image_paths_df.index[self.image_paths_df[2]==(example_name[:-2]+'.jpg')].values[0]
+        if index_found == None:
+            print('ERROR: cannot find image index')
 
-        # pdb.set_trace()
-
+        # find right image bbox
+        df_bbox = self.bboxes_df.iloc[[index_found]]
+        bbox_x = df_bbox[1].values[0]
+        bbox_y = df_bbox[2].values[0]
+        bbox_w = df_bbox[3].values[0]
+        bbox_h = df_bbox[4].values[0]
+        
         right_image = bytes(np.array(example['img']))
         right_embed = np.array(example['embeddings'], dtype=float)
-        wrong_image = bytes(np.array(self.find_wrong_image(example['class'])))
+        wrong_name, wrong_example = self.find_wrong_image(exmaple['class']) 
+        # wrong_image = bytes(np.array(self.find_wrong_image(example['class'])))
+        wrong_image = bytes(np.array(wrong_example)))
         inter_embed = np.array(self.find_inter_embed())
+
+        # find wrong image bbox
+        index_found_wrong = self.image_paths_df.index[self.image_paths_df[2]==(wrong_name[:-2]+'.jpg')].values[0]]
+        if index_found_wrong == None:
+            print('ERROR: cannot find wrong image')
+
+        df_bbox_wrong = self.bboxes_df.iloc[[index_found_wrong]]
+        wrong_bbox_x = df_bbox_wrong[1].values[0]
+        wrong_bbox_y = df_bbox_wrong[2].values[0]
+        wrong_bbox_w = df_bbox_wrong[3].values[0]
+        wrong_bbox_h = df_bbox_wrong[4].values[0]
 
         byte_right_image = io.BytesIO(right_image)
         byte_wrong_image = io.BytesIO(wrong_image)
 
-        right_image = Image.open(byte_right_image).resize((64, 64))
-        wrong_image = Image.open(byte_wrong_image).resize((64, 64))
+        right_image = Image.open(byte_right_image)
+        wrong_image = Image.open(byte_wrong_image)
+        
+        right_image = crop_image(right_image, bbox=[bbox_x, bbox_y, bbox_w, bboxh]) 
+        wrong_image = crop_image(wrong_image, bbox=[wrong_bbox_x, wrong_bbox_y, wrong_bbox_w, wrong_bboxh]) 
 
-        right_image128 = Image.open(byte_right_image).resize((128, 128))
-        wrong_image128 = Image.open(byte_wrong_image).resize((128, 128))
+        # right_image = Image.open(byte_right_image).resize((64, 64))
+        # wrong_image = Image.open(byte_wrong_image).resize((64, 64))
+        #right_image128 = Image.open(byte_right_image).resize((128, 128))
+        #wrong_image128 = Image.open(byte_wrong_image).resize((128, 128))
+
+        right_image = transforms.Scale(64)(right_image)
+        wrong_image = transforms.Scale(64)(wrong_image)
+        
+        right_image128 = transforms.Scale(128)(right_image)
+        wrong_image128 = transforms.Scale(128)(wrong_image) 
 
         right_image = self.validate_image(right_image)
         wrong_image = self.validate_image(wrong_image)
@@ -85,7 +135,7 @@ class Text2ImageDataset(Dataset):
         _category = example['class']
 
         if _category != category:
-            return example['img']
+            return example_name, example['img']
 
         return self.find_wrong_image(category)
 
